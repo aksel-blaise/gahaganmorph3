b8ddd72b515019813b41ed3f5d17087d21c99a5c
Robert Z. Selden, Jr <zac_selden@pm.me>
2020-10-22 06:55:21 -0500

    <init hist>

--- supp/density.R
+++ supp/density.R
@@ -11,15 +11,38 @@ site <- qdata$sitename
 # density plot for maxl by site
 ggplot(data, aes(x = length, fill = sitename, colour = sitename)) +
   geom_density(alpha = .5) +
-  scale_fill_manual(values = wes_palette("Cavalcanti1")) +
-  scale_colour_manual(values = wes_palette("Cavalcanti1")) +
+  scale_fill_manual(values = wes_palette("IsleofDogs1")) +
+  scale_colour_manual(values = wes_palette("IsleofDogs1")) +
   xlim(0,350) +
   labs(x = 'Max Length', y = 'density')
 
 # density plot for maxl by mort
 ggplot(data, aes(x = length, fill = mortuary, colour = mortuary)) +
   geom_density(alpha = .5) +
-  scale_fill_manual(values = wes_palette("Cavalcanti1")) +
-  scale_colour_manual(values = wes_palette("Cavalcanti1")) +
+  scale_fill_manual(values = wes_palette("IsleofDogs1")) +
+  scale_colour_manual(values = wes_palette("IsleofDogs1")) +
   xlim(0,350) +
   labs(x = 'Max Length', y = 'density')
+
+# density plot for maxl by context
+ggplot(data, aes(x = length, fill = context, colour = context)) +
+  geom_density(alpha = .5) +
+  scale_fill_manual(values = wes_palette("IsleofDogs1")) +
+  scale_colour_manual(values = wes_palette("IsleofDogs1")) +
+  xlim(0,350) +
+  labs(x = 'Max Length', y = 'density')
+
+
+## Barplot
+
+1 <- ggplot(data, aes(x = length)) +
+            geom_histogram(binwidth = 1, fill="#69b3a2", color="#e9ecef", alpha=0.9)
+
+2 <- ggplot(data, aes(x = length)) +
+            geom_histogram(binwidth = 2, fill="#69b3a2", color="#e9ecef", alpha=0.9)
+
+3 <- ggplot(data, aes(x = length)) +
+            geom_histogram(binwidth = 3, fill="#69b3a2", color="#e9ecef", alpha=0.9)
+
+3 <- ggplot(data, aes(x = length)) +
+            geom_histogram(binwidth = 3, fill="#69b3a2", color="#e9ecef", alpha=0.9)
